80b664e9221f6de94a28c87df42387f42116f273
Dharmesh2501 <dharmeshp2003@gmail.com>
2023-08-29 22:47:23 +0530

    added move directions

--- chandrayan4.py
+++ chandrayan4.py
@@ -33,7 +33,33 @@ class Spacecraft:
         elif self.direction == 'Down':
             self.position = (x, y, z + 1)
 
+    def turn_left(self):
+        if self.direction == 'N':
+            self.direction = 'W'
+        elif self.direction == 'S':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'N'
+        elif self.direction == 'W':
+            self.direction = 'S'
 
+    def turn_right(self):
+        if self.direction == 'N':
+            self.direction = 'E'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'W':
+            self.direction = 'N'
+
+    def turn_up(self):
+        if self.direction != 'Up':
+            self.direction = 'Up'
+
+    def turn_down(self):
+        if self.direction != 'Down':
+            self.direction = 'Down'
 
     def execute_commands(self, commands):
         for command in commands:
@@ -41,12 +67,19 @@ class Spacecraft:
                 self.move_forward()
             elif command == 'b':
                 self.move_backward()
-            
-          
+            elif command == 'l':
+                self.turn_left()
+            elif command == 'r':
+                self.turn_right()
+            elif command == 'u':
+                self.turn_up()
+            elif command == 'd':
+                self.turn_down()
 
 
 # Example usage
 starting_position = (0, 0, 0)
 initial_direction = 'N'
-commands = ["f", "r"]
+#commands = ["f", "r", "u", "b", "l"]
+
 chandrayaan_3 = Spacecraft(*starting_position, initial_direction)
